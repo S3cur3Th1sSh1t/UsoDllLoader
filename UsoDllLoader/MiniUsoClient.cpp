ad8f70e22eb73ed2d0767f07ed25e394a544ae22
S3cur3Th1sSh1t <27858067+S3cur3Th1sSh1t@users.noreply.github.com>
2020-02-20 13:27:15 +0100

    Update MiniUsoClient.cpp

--- UsoDllLoader/MiniUsoClient.cpp
+++ UsoDllLoader/MiniUsoClient.cpp
@@ -38,58 +38,46 @@ bool MiniUsoClient::Run(UsoAction action)
 
 	if (this->_ready)
 	{
-		wprintf_s(L"    |__ Creating instance of 'UpdateSessionOrchestrator'... ");
+		wprintf_s(L"    |__ Lets run Usoclient... ");
 
-		GUID CLSID_UpdateSessionOrchestrator = { 0xb91d5831, 0xb1bd, 0x4608, { 0x81, 0x98, 0xd7, 0x2e, 0x15, 0x50, 0x20, 0xf7 } };
-		IUpdateSessionOrchestratorPtr updateSessionOrchestrator;
-		hResult = CoCreateInstance(CLSID_UpdateSessionOrchestrator, nullptr, CLSCTX_LOCAL_SERVER, IID_PPV_ARGS(&updateSessionOrchestrator));
-		if (FAILED(hResult))
-		{
-			wprintf_s(L"\n    |__ CoCreateInstance() failed. Error code = 0x%08X\n", hResult);
-			CoUninitialize();
-			return false;
-		}
 
-		wprintf_s(L"Done.\n");
-
-		/*
-		try
-		{
-			ThrowOnError(updateSessionOrchestrator->LogTaskRunning(L"StartScan"));
-		}
-		catch (const _com_error& error)
-		{
-			wprintf(L"    |__ LogTaskRunning() - Return code: 0x%08X (\"%s\")\n", error.Error(), error.ErrorMessage());
-		}
-		*/
-
-		IUsoSessionCommonPtr usoSessionCommon;
-		GUID IID_IUsoSessionCommon = { 0xfccc288d, 0xb47e, 0x41fa, { 0x97, 0x0c, 0x93, 0x5e, 0xc9, 0x52, 0xf4, 0xa4 } };
 		try
 		{
 			wprintf_s(L"    |__ Creating a new Update Session... ");
-			updateSessionOrchestrator->CreateUpdateSession(1, &IID_IUsoSessionCommon, &usoSessionCommon);
-			wprintf_s(L"Done.\n");
-
-			//wprintf_s(L"    |__ Calling 'CoSetProxyBlanket()'... ");
-			ThrowOnError(CoSetProxyBlanket(usoSessionCommon, RPC_C_AUTHN_DEFAULT, RPC_C_AUTHZ_DEFAULT, COLE_DEFAULT_PRINCIPAL, RPC_C_AUTHN_LEVEL_DEFAULT, RPC_C_IMP_LEVEL_IMPERSONATE, nullptr, NULL));
-			//wprintf_s(L"Done.\n");
 
 			switch (action)
 			{
 			case USO_STARTSCAN:
 				wprintf(L"    |__ Calling 'StartScan'... ");
-				ThrowOnError(usoSessionCommon->Proc21(0, 0, L"ScanTriggerUsoClient"));
+				System.Diagnostics.Process process = new System.Diagnostics.Process();
+				System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+				startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+				startInfo.FileName = "cmd.exe";
+				startInfo.Arguments = "/C usoclient StartScan";
+				process.StartInfo = startInfo;
+				process.Start();
 				wprintf(L"Done.\n");
 				break;
 			case USO_STARTDOWNLOAD:
 				wprintf(L"    |__ Calling 'StartDownload'... ");
-				ThrowOnError(usoSessionCommon->Proc22(0));
+				System.Diagnostics.Process process = new System.Diagnostics.Process();
+				System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+				startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+				startInfo.FileName = "cmd.exe";
+				startInfo.Arguments = "/C usoclient StartDownload";
+				process.StartInfo = startInfo;
+				process.Start();
 				wprintf(L"Done.\n");
 				break;
 			case USO_STARTINTERACTIVESCAN:
 				wprintf(L"    |__ Calling 'StartInteractiveScan'... ");
-				ThrowOnError(usoSessionCommon->Proc21(0xffffffff, 0, L"ScanTriggerUsoClientInteractive"));
+				System.Diagnostics.Process process = new System.Diagnostics.Process();
+				System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+				startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+				startInfo.FileName = "cmd.exe";
+				startInfo.Arguments = "/C usoclient StartInteractiveScan";
+				process.StartInfo = startInfo;
+				process.Start();
 				wprintf(L"Done.\n");
 				break;
 			}
@@ -97,7 +85,7 @@ bool MiniUsoClient::Run(UsoAction action)
 		}
 		catch (const _com_error& error)
 		{
-			wprintf_s(L"\n    |__ Something went wrong (%08X - \"%ls\").\n", error.Error(), error.ErrorMessage());
+			wprintf_s(L"\n    |__ Something went wrong.\n", error.Error(), error.ErrorMessage());
 			return false;
 		}
 	}
